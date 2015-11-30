d5c53bc4fb61b0bfe29fffe30813b76cb03d3796
Becky Bair <beckyhowen@gmail.com>
2015-11-29 21:57:40 -0600

    Handle constraints with nice error messages

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,3 +1,24 @@
+def three_letters(input)
+  if input.length > 3
+		# return true if input is 3 or less characters
+    # and false otherwise
+    raise Exception.new("** Word must be less than or equal to 3 characters. **")
+  end
+end
+
+def distinct_letters(input)
+   letter_array = input.chars
+   unique_letters = letter_array.uniq
+   if unique_letters.length < letter_array.length
+     raise Exception.new("** Word must have distinct letters please! **")
+   end
+end
+
+def valid_input(input)
+	three_letters(input)
+	distinct_letters(input)
+end
+
 get '/' do
   erb :index
 end
@@ -10,5 +31,11 @@ end
 
 post '/' do
   @word = params[:word]
-  redirect "/anagrams/#{@word}"
+  begin 
+		valid_input(@word)
+    redirect "/anagrams/#{@word}"
+  rescue Exception => error
+    @error = error.message
+    erb :index
+  end
 end
